d5db54d54252090105bbe4ebb58053df287c1dd4
philippetsongo <philippetsongo90@gmail.com>
2023-12-04 12:21:57 +0200

    user log out

--- articles/urls.py
+++ articles/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
+# from django.urls import re_path
+
 from . import views
-from django.urls import re_path
 
 app_name = 'articles'
 
 urlpatterns = [
     path('', views.article_list, name="list"),
+    path('create/', views.article_create, name="create"),
     re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 
 ]
