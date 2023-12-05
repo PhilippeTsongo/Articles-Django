8e63765905b9854ab9b9ba2343bff5283e046f9e
philippetsongo <philippetsongo90@gmail.com>
2023-12-05 18:11:21 +0200

    -

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from . import forms
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def article_list(request):
@@ -12,3 +13,17 @@ def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+@login_required(login_url="/accounts/login")
+def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST)
+        if form.is_valid():
+            #save in the db
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', {'form': form})
